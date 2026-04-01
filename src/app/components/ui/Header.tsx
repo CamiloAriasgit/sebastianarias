7dac65702a5423983f22a5923b61ac09ff85f321
SCAB <camiloab203@gmail.com>
2026-03-31 19:00:14 -0500

    feat: header hero section

---
+++ src/app/components/ui/Header.tsx
@@ -0,0 +1,35 @@
+export default function Navbar() {
+  return (
+    <header className="fixed top-4 left-0 right-0 z-50 flex justify-center px-4">
+      <nav className="flex w-full max-w-2xl items-center justify-between rounded-full bg-neutral-200/60 px-6 py-2.5 backdrop-blur-md ">
+        
+        <div className="flex items-center gap-2">
+          <span className="text-sm font-bold tracking-tight text-neutral-900">
+            SCAB
+          </span>
+        </div>
+
+        <ul className="hidden items-center gap-6 sm:flex">
+          <li>
+            <a href="#servicios" className="text-xs font-medium text-neutral-500 transition-colors hover:text-neutral-900">
+              Servicios
+            </a>
+          </li>
+          <li>
+            <a href="#proyectos" className="text-xs font-medium text-neutral-500 transition-colors hover:text-neutral-900">
+              Proyectos
+            </a>
+          </li>
+        </ul>
+
+        {/* CTA */}
+        <a 
+          href="#contacto" 
+          className="rounded-full bg-neutral-900 px-4 py-2 text-xs font-semibold text-white transition-all hover:bg-neutral-800 active:scale-95"
+        >
+          Contacto
+        </a>
+      </nav>
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/sections/Hero";
+import Header from "./components/ui/Header";
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
+        <Header/>
         <Hero />
       </main>
     </div>
