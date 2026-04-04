8c1c7b7237f11e881588d52200dab3a66898f8f6
SCAB <camiloab203@gmail.com>
2026-04-04 12:10:31 -0500

    feat: header icon globe

--- src/app/components/ui/Header.tsx
+++ src/app/components/ui/Header.tsx
@@ -1,13 +1,22 @@
+"use client";
+
+import React, { useState } from "react";
+import { Globe } from "lucide-react";
+
 export default function Navbar() {
   return (
     <header className="fixed top-4 left-0 right-0 z-50 flex justify-center px-4">
-      <nav className="flex w-full max-w-2xl items-center justify-between rounded-full bg-gray-200/60 pl-6 pr-3 py-2.5 backdrop-blur-md ">
+      <nav className="flex w-full max-w-2xl items-center justify-between rounded-full bg-gray-200/60 pl-3 pr-3 py-2.5 backdrop-blur-md ">
+
+      <div className="flex items-center rounded-full">
+        <Globe className="h-10 w-10 text-gray-300/80" />
+      </div>
         
-        <div className="flex items-center gap-2">
+        {/*<div className="flex items-center gap-2">
           <span className="text-sm font-mono tracking-tight text-neutral-900">
             Scab
           </span>
-        </div>
+        </div>*/}
 
         <ul className="hidden items-center gap-6 sm:flex">
           <li>
