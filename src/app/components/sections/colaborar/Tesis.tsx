8c20cb306c33619ceae33d3bc9cab907ae116f49
SCAB <camiloab203@gmail.com>
2026-04-13 20:17:53 -0500

    feat: tesis font lg pt-5

--- src/app/components/sections/colaborar/Tesis.tsx
+++ src/app/components/sections/colaborar/Tesis.tsx
@@ -7,11 +7,11 @@ export default function Tesis() {
     <section className="w-full bg-neutral-950 px-4 py-24 sm:py-32 border-t border-white/5">
       <div className="mx-auto max-w-3xl">
         
-        <h2 className="font-sans text-3xl font-bold tracking-tight leading-[0.9] text-neutral-200 sm:text-4xl mb-12">
+        <h2 className="font-sans text-3xl font-bold tracking-tight  text-neutral-200 sm:text-4xl mb-12">
           La mayoría de SaaS fallan por una razón
         </h2>
 
-        <div className="space-y-1 font-sans text-2xl font-bold leading-[0.9] text-neutral-500">
+        <div className="space-y-1 font-sans text-lg md:text-2xl font-bold leading-[0.9] text-neutral-500">
           <p className="text-neutral-200 font-semibold">
             No fallan por falta de código.
           </p>
@@ -20,7 +20,7 @@ export default function Tesis() {
             Fallan porque nadie validó si realmente eran necesarios.
           </p>
 
-          <p className="pt-10 bg-gradient-to-r from-blue-500 via-pink-500 to-yellow-500 bg-clip-text text-transparent">
+          <p className="pt-5 bg-gradient-to-r from-blue-500 via-pink-500 to-yellow-500 bg-clip-text text-transparent">
             Este proceso es diferente.
           </p>
 
