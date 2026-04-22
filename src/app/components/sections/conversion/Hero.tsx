6706b792b486a548daaa6ea26b81eab7fe4a355f
SCAB <camiloab203@gmail.com>
2026-04-22 16:00:44 -0500

    feat: conversion hero white section

--- src/app/components/sections/conversion/Hero.tsx
+++ src/app/components/sections/conversion/Hero.tsx
@@ -7,9 +7,9 @@ export default function Hero({ setBg }: { setBg: (colors: any) => void }) {
   const isInView = useInView(ref, { amount: 0.5 });
 
   const colors = { 
-    from: '#4262ab', 
-    via: '#97a9b1', // Azul medianoche muy sutil
-    to: '#a98672' 
+    from: '#f8f8ff', 
+    via: '#f8f8ff', // Azul medianoche muy sutil
+    to: '#f8f8ff' 
   };
 
   useEffect(() => {
@@ -18,13 +18,13 @@ export default function Hero({ setBg }: { setBg: (colors: any) => void }) {
 
   return (
     <section ref={ref} className="min-h-[100svh] flex flex-col items-center justify-center text-center px-4">
-      <span className=" bg-white/10 rounded-full px-2 py-1 text-white text-sm mb-4 shadow-inner shadow-white/30">
+      <span className=" bg-black/5 rounded-full px-2 py-1 text-neutral-900 text-sm mb-4 shadow-inner shadow-black/0">
         Conversion
       </span>
-      <h1 className="text-white text-5xl md:text-7xl tracking-tight max-w-4xl leading-[0.9]">
+      <h1 className="text-black text-4xl md:text-7xl tracking-tight max-w-4xl font-bold leading-[0.9]">
         Tu negocio es real. Tu infraestructura digital no.
       </h1>
-      <p className="text-white mt-6 max-w-xl text-lg leading-[1.2]">
+      <p className="text-neutral-500 mt-6 max-w-xl text-lg leading-[1.2]">
         Desplegamos sistemas de conversión de alta velocidad para empresas que no pueden permitirse perder clientes.
       </p>
     </section>
