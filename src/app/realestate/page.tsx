cdedcba24776b7f1667fd56764cf5a56da3891c0
SCAB <camiloab203@gmail.com>
2026-05-11 19:28:38 -0500

    feat: realestate medicion section init

--- src/app/realestate/page.tsx
+++ src/app/realestate/page.tsx
@@ -3,6 +3,7 @@ import Footer from "../components/ui/conversion/ConversionHeader";
 import Hero from "../components/sections/realestate/Hero";
 import Segunda from "../components/sections/realestate/Segunda";
 import Tercera from "../components/sections/realestate/Tercera";
+import Medicion from "../components/sections/realestate/Medicion";
 
 
 export default function Home() {
@@ -20,8 +21,9 @@ export default function Home() {
         <main className="relative w-full h-screen overflow-y-auto overflow-x-hidden snap-y snap-mandatory scroll-smooth">
             <Header />
             <div className="snap-start w-full"><Hero /></div>
-            <div className="snap-start w-full"><Segunda /></div>
             <div className="snap-start w-full"><Tercera /></div>
+            <div className="snap-start w-full"><Segunda /></div>
+            <div className="snap-start w-full"><Medicion /></div>
         </main>
     );
 }
