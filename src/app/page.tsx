4627d56dab305cd74600eb7f83bbf70c62e03d4b
SCAB <camiloab203@gmail.com>
2026-06-01 20:12:17 -0500

    feat: new hero 3 v

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 // app/page.tsx
+import Header from './components/layout/Header'
 import Hero from './components/sections/Hero'
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      {/* <Problem /> */}
-      {/* <Service /> */}
-      {/* <Process /> */}
-      {/* <Demo /> */}
-      {/* <Pricing /> */}
-      {/* <FAQ /> */}
-      {/* <CTAFinal /> */}
-    </main>
+    <>
+      <Header />
+      <main>
+        <Hero />
+        {/* <Problem /> */}
+        {/* <Service /> */}
+        {/* <Process /> */}
+        {/* <Demo /> */}
+        {/* <Pricing /> */}
+        {/* <FAQ /> */}
+        {/* <CTAFinal /> */}
+      </main>
+    </>
   )
 }
